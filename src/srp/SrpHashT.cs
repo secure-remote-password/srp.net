477364269b2db11a6c3798d287d01da7325c6b6c
yallie <yallie@yandex.ru>
2018-06-06 19:17:27 +0300

    Added the missing file.

---
+++ src/srp/SrpHashT.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace SecureRemotePassword
+{
+	/// <summary>
+	/// Hashing algorithms for the SRP-6a protocol.
+	/// </summary>
+	/// <typeparam name="T">Hash algorithm type.</typeparam>
+	public class SrpHash<T> : SrpHash
+		where T : HashAlgorithm
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SrpHash{T}"/> class.
+		/// </summary>
+		public SrpHash()
+			: base(CreateHasher())
+		{
+		}
+
+		/// <summary>
+		/// Creates the hasher of the given type <typeparamref name="T"/>.
+		/// </summary>
+		public static T CreateHasher() => (T)CreateHasher(typeof(T).Name);
+	}
+}
