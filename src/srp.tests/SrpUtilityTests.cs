d1c364d6a816e9ef2d8995fee4c5ef922001c71f
yallie <yallie@yandex.ru>
2020-01-23 09:23:07 +0300

    Added utility methods to validate salt and verifier values, #8.

---
+++ src/srp.tests/SrpUtilityTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using NUnit.Framework;
+
+namespace SecureRemotePassword.Tests
+{
+	/// <summary>
+	/// <see cref="SrpServer"/> tests.
+	///</summary>
+	[TestFixture]
+	public class SrpUtilityTests
+	{
+		[TestCase(SrpConstants.SafePrime1024, SrpConstants.Generator1024)]
+		[TestCase(SrpConstants.SafePrime1536, SrpConstants.Generator1536)]
+		[TestCase(SrpConstants.SafePrime2048, SrpConstants.Generator2048)]
+		[TestCase(SrpConstants.SafePrime3072, SrpConstants.Generator3072)]
+		[TestCase(SrpConstants.SafePrime4096, SrpConstants.Generator4096)]
+		[TestCase(SrpConstants.SafePrime6144, SrpConstants.Generator6144)]
+		[TestCase(SrpConstants.SafePrime8192, SrpConstants.Generator8192)]
+		public void SrpImplementationGeneratesValidSaltAndVerifier(string prime, string generator)
+		{
+			SrpImplementationGeneratesValidSaltAndVerifier<MD5>(prime, generator);
+			SrpImplementationGeneratesValidSaltAndVerifier<SHA1>(prime, generator);
+			SrpImplementationGeneratesValidSaltAndVerifier<SHA256>(prime, generator);
+			SrpImplementationGeneratesValidSaltAndVerifier<SHA384>(prime, generator);
+			SrpImplementationGeneratesValidSaltAndVerifier<SHA512>(prime, generator);
+		}
+
+		private void SrpImplementationGeneratesValidSaltAndVerifier<T>(string prime, string generator)
+			where T : HashAlgorithm
+		{
+			// set up parameters
+			var parameters = SrpParameters.Create<T>(prime, generator);
+			var client = new SrpClient(parameters);
+			var server = new SrpServer(parameters);
+
+			// sign up
+			var salt = client.GenerateSalt();
+			var privateKey = client.DerivePrivateKey(salt, "root", "123");
+			var verifier = client.DeriveVerifier(privateKey);
+
+			// verify generated values
+			Assert.IsTrue(parameters.IsValidSalt(salt));
+			Assert.IsTrue(parameters.IsValidVerifier(verifier));
+		}
+	}
+}
---
+++ src/srp/SrpUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecureRemotePassword
+{
+	/// <summary>
+	/// Utility methods, extension methods, etc.
+	/// </summary>
+	public static class SrpUtility
+	{
+		/// <summary>
+		/// Checks if the given hexadecimal string is a valid padded SRP integer.
+		/// </summary>
+		/// <param name="hexString">Integer value represented as hexadecimal string.</param>
+		/// <param name="requiredLength">Required length of the hexadecimal string.</param>
+		/// <returns>True, if the value is valid.</returns>
+		public static bool IsValidInteger(string hexString, int requiredLength)
+		{
+			if (string.IsNullOrEmpty(hexString) || hexString.Length != requiredLength)
+			{
+				return false;
+			}
+
+			try
+			{
+				var tmp = new SrpInteger(hexString);
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Checks if the given salt value is a valid padded hexadecimal string.
+		/// </summary>
+		/// <param name="parameters">SRP parameters.</param>
+		/// <param name="salt">Hexadecimal salt string.</param>
+		/// <returns>True, if the salt value is valid.</returns>
+		public static bool IsValidSalt(this SrpParameters parameters, string salt) => IsValidInteger(salt, parameters.HashSizeBytes * 2);
+
+		/// <summary>
+		/// Checks if the given verifier value is a valid padded hexadecimal string.
+		/// </summary>
+		/// <param name="parameters">SRP parameters.</param>
+		/// <param name="verifier">Password verifier.</param>
+		/// <returns>True, if the verifier value is valid.</returns>
+		public static bool IsValidVerifier(this SrpParameters parameters, string verifier) => IsValidInteger(verifier, parameters.PaddedLength);
+	}
+}
