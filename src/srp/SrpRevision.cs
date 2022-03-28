79ef8d66cec34e43350632d6d3f86698f10b9581
yallie <yallie@yandex.ru>
2022-03-29 01:42:03 +0300

    Added missing XML comments.

--- src/srp/SrpRevision.cs
+++ src/srp/SrpRevision.cs
@@ -1,9 +1,23 @@
 namespace SecureRemotePassword
 {
+	/// <summary>
+	/// SRP protocol revisions.
+	/// </summary>
 	public enum SrpRevision
 	{
+		/// <summary>
+		/// SRP-3 protocol.
+		/// </summary>
 		Three = 1,
+
+		/// <summary>
+		/// SRP-6 protocol.
+		/// </summary>
 		Six = 2,
-		SixA = 3
+
+		/// <summary>
+		/// SRP-6a protocol, the default.
+		/// </summary>
+		SixA = 3,
 	}
 }
